5726bfd06f9f18b57497192ba81191d597fda091
HenriqueV01 <henrique.unibratec01@gmail.com>
2025-04-03 12:42:00 -0300

    Rota para o componente Home.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
   {
-    path: '', component: NavComponent
+    path: '', component: NavComponent, children: [
+      // { path: '', redirectTo: 'home', pathMatch: 'full' }, // Redireciona para /home dentro de nav.
+      {path: 'home', component: HomeComponent}
+    ]
   }
 
 ];
